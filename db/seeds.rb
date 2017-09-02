10eada57a06084a3a909bd0dde7538a58caf9ea2
gcampo88 <gigi.campo@gmail.com>
2017-09-02 12:36:02 -0400

    add 2016 pairs

--- db/seeds.rb
+++ db/seeds.rb
@@ -98,3 +98,43 @@ PreviousPair.create!(giver: meg, recipient: betsy_p, year: 2015)
 PreviousPair.create!(giver: mac, recipient: harry_4, year: 2015)
 PreviousPair.create!(giver: gretta, recipient: katie_a, year: 2015)
 PreviousPair.create!(giver: lucy, recipient: tim, year: 2015)
+
+# 2016:
+# campos
+PreviousPair.create!(giver: betsy_c, recipient: chris, year: 2016)
+PreviousPair.create!(giver: jack, recipient: laurenricky, year: 2016)
+PreviousPair.create!(giver: katie_c, recipient: mac, year: 2016)
+PreviousPair.create!(giver: warren, recipient: laurie, year: 2016)
+PreviousPair.create!(giver: gigi, recipient: suzy, year: 2016)
+PreviousPair.create!(giver: max, recipient: billy, year: 2016)
+PreviousPair.create!(giver: will, recipient: louise, year: 2016)
+
+# alvies
+PreviousPair.create!(giver: harry_3, recipient: martha, year: 2016)
+PreviousPair.create!(giver: katie_a, recipient: will, year: 2016)
+PreviousPair.create!(giver: harry_4, recipient: max, year: 2016)
+PreviousPair.create!(giver: louise, recipient: sarah, year: 2016)
+PreviousPair.create!(giver: laurenricky, recipient: gretta, year: 2016)
+PreviousPair.create!(giver: lily, recipient: betsy_p, year: 2016)
+
+# littlefields
+PreviousPair.create!(giver: suzy, recipient: harry_4, year: 2016)
+PreviousPair.create!(giver: josh, recipient: tim, year: 2016)
+PreviousPair.create!(giver: betsy_p, recipient: deniz, year: 2016)
+PreviousPair.create!(giver: dan, recipient: warren, year: 2016)
+PreviousPair.create!(giver: billy, recipient: katie_c, year: 2016)
+
+# lex groveses
+PreviousPair.create!(giver: martha, recipient: meg, year: 2016)
+PreviousPair.create!(giver: chris, recipient: jack, year: 2016)
+PreviousPair.create!(giver: sarah, recipient: lucy, year: 2016)
+PreviousPair.create!(giver: tim, recipient: josh, year: 2016)
+PreviousPair.create!(giver: deniz, recipient: katie_a, year: 2016)
+
+
+# Vermont Groveses
+PreviousPair.create!(giver: laurie, recipient: gigi, year: 2016)
+PreviousPair.create!(giver: meg, recipient: harry_3, year: 2016)
+PreviousPair.create!(giver: mac, recipient: lily, year: 2016)
+PreviousPair.create!(giver: gretta, recipient: dan, year: 2016)
+PreviousPair.create!(giver: lucy, recipient: betsy_c, year: 2016)
